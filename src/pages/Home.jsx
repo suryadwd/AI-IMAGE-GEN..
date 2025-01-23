8a22058c4173dbbc671105910d8a9a19eb541522
Suryakant <surajdwivedi644@gmail.com>
2025-01-24 00:43:24 +0530

    testimaonial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../components/Header'
 import Steps from '../components/Steps'
 import Description from '../components/Description'
+import Testi from '../components/Testi'
 const Home = () => {
   return (
     <div>
      <Header />
     <Steps />
     <Description />
+    <Testi />
     </div>
   )
 }
