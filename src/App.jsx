93252b78fe36c875550dd36eb3c42b1c07ceb4c0
Suryakant <surajdwivedi644@gmail.com>
2025-01-23 16:18:57 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Buy from "./pages/Buy";
+import Result from "./pages/Result";
+import Navbar from "./components/Navbar";
 const App = () => {
   return (
-   <>
-   
-   
-   
-   </>
-  )
-}
+    <div className="px-4 sm:px-10 md:px-14 lg:px-21 min-h-screen bg-gradient-to-b from-teal-100 to-orange-100">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/buy" element={<Buy />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
