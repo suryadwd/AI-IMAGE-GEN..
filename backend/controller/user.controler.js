35a0fddccc813658038b64e9dc01a94151686dac
Suryakant <surajdwivedi644@gmail.com>
2025-01-25 13:15:41 +0530

    login api

--- backend/controller/user.controler.js
+++ backend/controller/user.controler.js
@@ -9,7 +9,9 @@ export const register = async (req, res) => {
     
     const {name, email, password} = req.body
 
-    if(!name||!email||!password) return res.status(400).json({success:false, message:"All fields are required"})
+    if(!name||!email) return res.status(400).json({success:false, message:"All fields are required"})
+
+    if(password.length < 6 ||!password ) return res.status(400).json({success:false, message:"Passwors must be  greater then 6 character"})
 
     const existingUser = await User.findOne({email})  
 
@@ -19,17 +21,50 @@ export const register = async (req, res) => {
 
     const newUser = new User({name,email,password:hashPass})
 
-    newUser.save()
+    await newUser.save()
 
     const payload = {id : newUser._id}
 
-    genTokllenCookie(payload, res)
+    genTokenCookie(payload, res)
 
-    return res.status(201).json({success:true,message:"user created", newUser})
+    return res.status(201).json({success:true,message:"user created"})
 
   } catch (error) {
-    return res.status(500).json({success:false, message:"error in login handler"})
     console.log(error)
+    return res.status(500).json({success:false, message:"error in login handler"})
   }
 
 }
+
+
+export const login = async (req, res) => {
+
+  try {
+    
+    const {email, password} = req.body
+
+    if(!email||!password) return res.status(400).json({success:false, message:"All fields are required"})
+
+    if(password.length < 6 ||!password ) return res.status(400).json({success:false, message:"Passwors must be  greater then 6 character"})
+
+    const existingUser = await User.findOne({email})  
+
+    if(!existingUser) return res.status(400).json({success:false, message:"User does not exist"})
+
+    const isMatch = await bcrypt.compare(password, existingUser.password)
+
+    if(!isMatch) return res.status(400).json({success:false, message:"Password Incorrect"})
+
+    const payload = {id : existingUser._id}
+
+    genTokenCookie(payload, res)
+
+    return res.status(200).json({success:true,message:"Login", newUser})
+
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({success:false, message:"error in login handler"})
+  }
+
+}
+
